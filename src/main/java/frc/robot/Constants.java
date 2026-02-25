cc266125d743d48aa61d15866c24f4e61c3c75c5
Clark Wang <clarkywang09@gmail.com>
2026-02-25 15:21:01 -0600

    Hood PID Base

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+public class Constants {
+    public static class HoodConstants {
+        public static double kP = 0;
+        public static double kI = 0;
+        public static double kD = 0;
+
+        public static double kS = 0;
+        public static double kG = 0;
+        public static double kV = 0;
+
+    }
+}
