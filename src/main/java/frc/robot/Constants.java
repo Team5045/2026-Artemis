e838ba8a5ffaedaac6886c7f4c4516038689df2b
TheGraytOne <graytonfaust@gmail.com>
2026-02-27 20:14:49 -0600

    Developed intakePID subsystem with it's own constants, set function, profiled PID, and feedforward

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,4 +21,17 @@ public class Constants {
         public static double kV = 0;
 
     }
+    public static class IntakeConstants {
+        public static double kP = 0;
+        public static double kI = 0;
+        public static double kD = 0;
+
+        public static double kS = 0;
+        public static double kG = 0;
+        public static double kV = 0;
+        public static double kA = 0;
+
+        public static double tpV = 0;
+        public static double tpA = 0;
+    }
 }
--- src/main/java/frc/robot/subsystems/intakePID.java
+++ src/main/java/frc/robot/subsystems/intakePID.java
@@ -1,23 +1,25 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import com.ctre.phoenix6.hardware.TalonFX;
+import frc.robot.Constants.IntakeConstants;
 
 public class intakePID{
     TalonFX motor1;
-    TalonFX motor2;
-    PIDController pid;
-    ArmFeedforward feedforward;
+    ProfiledPIDController pid;
+    ArmFeedforward ff;
 
-    public intakePID(TalonFX motor1, TalonFX motor2){
-        this.motor1 = motor1;
-        this.motor2 = motor2;
-        this.pid = new PIDController(kP, kI, kD);
-        this.feedforward = new ArmFeedforward(kS, kG, kV, kA);
+    public intakePID(int id){
+        this.motor1 = new TalonFX(id);
+        this.pid = new ProfiledPIDController(IntakeConstants.kP, IntakeConstants.kI, IntakeConstants.kD, new TrapezoidProfile.Constraints(IntakeConstants.tpV, IntakeConstants.tpA));
+        this.ff = new ArmFeedforward(IntakeConstants.kS, IntakeConstants.kG, IntakeConstants.kV, IntakeConstants.kA);
     }
 
-    public void goDown(){
-
+    public void set(double setpoint){
+        double position = motor1.getPosition().getValueAsDouble();
+        double velocity = motor1.getVelocity().getValueAsDouble();
+        this.motor1.set(pid.calculate(position, setpoint) + ff.calculate(position, velocity));
     }
 }
